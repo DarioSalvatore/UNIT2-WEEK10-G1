3dceb84b3bc43eb217b8b7f93a78a7ea51c7bba7
DarioSalvatore <dariopay69@gmail.com>
2023-04-04 18:12:49 +0200

    Week10-g2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import MyNav from './components/MyNav'
-import MyFooter from './components/MyFooter'
-import MyJumbotron from './components/MyJumbotron'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
+import MyJumbotron from "./components/MyJumbotron";
 // import AllTheBooks from './components/AllTheBooks'
-import { Container } from 'react-bootstrap'
-import BookList from './components/BookList'
+import { Container } from "react-bootstrap";
+import BookList from "./components/BookList";
 
-import fantasy from './data/fantasy.json'
+import fantasy from "./data/fantasy.json";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
       <BookList books={fantasy} />
       <MyFooter />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,88 +1,86 @@
-import { Component } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
-class AddComment extends Component {
-  state = {
-    commentObj: {
-      comment: "",
-      rate: "1",
-      elementId: this.props.asin
-    }
-  };
+const AddComment = (props) => {
+  // state = {
+  //   commentObj: {
+  //     comment: "",
+  //     rate: "1",
+  //     elementId: this.props.asin
+  //   }
+  // };
 
-  sendComment = async e => {
-    e.preventDefault();
+  const [comment, setComment] = useState("");
+  const [rate, setRate] = useState("1");
+
+  const sendComment = async (e) => {
+    e.useEffect(() => {}, []);
 
     try {
-      const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/", {
-        method: "POST",
-        body: JSON.stringify(this.state.commentObj),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhZThlNTY4MzQzMTAwMTRkZWE3ZWYiLCJpYXQiOjE2ODA1MzM3MzMsImV4cCI6MTY4MTc0MzMzM30.BybWrL_zO0q23jXsrG8pA-yPWXt9QYBf4zZcRNL920U"
+      const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            comment: comment,
+            rate: rate,
+            elementId: props.asin,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhZThlNTY4MzQzMTAwMTRkZWE3ZWYiLCJpYXQiOjE2ODA1MzM3MzMsImV4cCI6MTY4MTc0MzMzM30.BybWrL_zO0q23jXsrG8pA-yPWXt9QYBf4zZcRNL920U",
+          },
         }
-      });
+      );
       if (response.ok) {
         // alert("Commento inviato");
 
         // ricrea la lista di commenti nel livello superiore (il padre: CommentArea)
         // attraverso la referenza della funzione fetchComments passata come prop a questo componente
-        this.props.fetchComments();
+        props.fetchComments();
 
         // resetta i campi (svuotarli)
-        this.setState({
-          commentObj: {
-            comment: "",
-            rate: "1",
-            elementId: this.props.asin
-          }
-        });
+        setComment("");
+        setRate("1");
       }
     } catch (error) {
       alert(error);
     }
   };
 
-  render() {
-    return (
-      <Form onSubmit={this.sendComment}>
-        <Form.Group className="mb-3" controlId="comment">
-          <Form.Label>Commento</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Inserisci il commento"
-            value={this.state.commentObj.comment}
-            onChange={e => this.setState({ commentObj: { ...this.state.commentObj, comment: e.target.value } })}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="comment">
-          <Form.Label>Voto</Form.Label>
-          <Form.Select
-            value={this.state.commentObj.rate}
-            onChange={e => {
-              this.setState({
-                commentObj: {
-                  ...this.state.commentObj,
-                  rate: e.target.value
-                }
-              });
-            }}
-          >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Form.Select>
-        </Form.Group>
+  return (
+    <Form onSubmit={sendComment}>
+      <Form.Group className="mb-3" controlId="comment">
+        <Form.Label>Commento</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Inserisci il commento"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="comment">
+        <Form.Label>Voto</Form.Label>
+        <Form.Select
+          value={rate}
+          onChange={(e) => {
+            setRate(e.target.value);
+          }}
+        >
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Form.Select>
+      </Form.Group>
 
-        <Button type="submit" variant="primary">
-          Invia commento
-        </Button>
-      </Form>
-    );
-  }
-}
+      <Button type="submit" variant="primary">
+        Invia commento
+      </Button>
+    </Form>
+  );
+};
 
 export default AddComment;
